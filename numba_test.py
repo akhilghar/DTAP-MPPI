01857875569f1280fc19516e612565f11738a0a8
akhilgharpurey <akhilghar@utexas.edu>
2026-02-05 14:56:02 -0600

    Vector addition test file

--- numba_test.py
+++ numba_test.py
@@ -1,3 +1,16 @@
-from numba import jit
+from numba import vectorize, cuda
 import numpy as np
-import time
+
+# Define a CUDA vectorized function
+@vectorize(['float32(float32, float32)'], target='cuda')
+def add_vectors_cuda(a, b):
+    return a + b
+
+# Create large arrays in NumPy (host memory)
+N = 100000
+A = np.ones(N, dtype=np.float32)
+B = np.ones(N, dtype=np.float32)
+
+# Numba automatically manages memory transfer and kernel execution
+C = add_vectors_cuda(A, B)
+print(C)
